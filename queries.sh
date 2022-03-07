10ffe546011f6ed5323d966c1ae7814bac4baf23
Charles Olupot <olupotcharles@gmail.com>
2022-03-07 16:51:05 +0300

    Testing

---
+++ queries.sh
@@ -0,0 +1,1 @@
+organisationunitid,uid,code,created,lastupdated,lastupdatedby,name,shortname,parentid,path,hierarchylevel,description,openingdate,closeddate,comment,url,contactperson,address,email,phonenumber,userid,translations,attributevalues
