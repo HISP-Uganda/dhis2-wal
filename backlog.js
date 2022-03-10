d0e5eaa0fd7ef7ffa6a0d8cdfb3d884d4555953e
Charles Olupot <olupotcharles@gmail.com>
2022-03-10 11:42:53 +0300

    Testing

--- backlog.js
+++ backlog.js
@@ -2,6 +2,7 @@ import * as pgwire from "pgwire";
 import _ from "lodash";
 import * as common from "./common.js";
 import * as dotenv from "dotenv";
+import * as df from "date-fns";
 
 dotenv.config();
 const hirarchy = {
@@ -12,6 +13,16 @@ const hirarchy = {
   4: "facility",
 };
 const args = process.argv.slice(2);
+
+function getDatesInRange(startDate, endDate) {
+  const date = new Date(startDate.getTime());
+  const dates = [];
+  while (date <= endDate) {
+    dates.push(new Date(date));
+    date.setDate(date.getDate() + 1);
+  }
+  return dates;
+}
 export const client = await pgwire.pgconnect({
   user: process.env.PG_USER,
   password: process.env.PG_PASSWORD,
@@ -19,12 +30,17 @@ export const client = await pgwire.pgconnect({
   port: process.env.PG_PORT,
   database: process.env.PG_DATABASE,
 });
-const limit = 10000;
+const dates = getDatesInRange(new Date(args[0]), new Date(args[1]))
+  .sort(df.compareAsc)
+  .map((d) => [
+    df.format(d, "yyyy-MM-dd"),
+    df.format(df.addDays(d, 1), "yyyy-MM-dd"),
+  ]);
 try {
-  for (let i = 0; i <= 683; i++) {
-    console.log(`Working on ${i}`);
+  for (const [start, end] of dates) {
+    console.log(`Working on ${start}`);
     const { results } = await client.query(
-      `${args[0]} limit ${limit} offset ${i * limit};`
+      `select o.uid ou,o.name,o.path,psi.programstageinstanceid::text,psi.uid,to_char(psi.created,'YYYY-MM-DD') created,to_char(psi.created,'MM') m,to_char(psi.lastupdated,'YYYY-MM-DD') lastupdated,programinstanceid::text,programstageid::text,attributeoptioncomboid::text,psi.deleted,psi.storedby,to_char(duedate,'YYYY-MM-DD') duedate,to_char(executiondate,'YYYY-MM-DD') executiondate,psi.organisationunitid::text,status,completedby,to_char(completeddate,'YYYY-MM-DD') completeddate,eventdatavalues->'bbnyNYD1wgS'->>'value' as vaccine,eventdatavalues->'LUIsbsm3okG'->>'value' as dose,assigneduserid::text,psi.createdbyuserinfo,psi.lastupdatedbyuserinfo from programstageinstance psi inner join organisationunit o using(organisationunitid) where psi.created >= '${start}' and psi.created < '${end}' and programstageid = 12715`
     );
     for (const { rows, columns } of results) {
       const data = rows.map((r) => {
@@ -47,11 +63,14 @@ try {
           })
         );
       });
-      await common.api.post(`wal/index?index=${args[1]}`, {
+      const { data: dt } = await common.api.post(`wal/index?index=${args[2]}`, {
         data,
       });
+
+      console.log(dt);
+
     }
-    console.log(`Finished working on ${i}`);
+    console.log(`Finished working on ${start}`);
   }
 } catch (error) {
   console.log(error.message);
