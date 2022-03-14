978e8450f2205f9442436c325b047eaca300db88
Charles Olupot <olupotcharles@gmail.com>
2022-03-14 10:24:54 +0300

    Scheduling

--- schdule.js
+++ schdule.js
@@ -15,21 +15,22 @@ const hirarchy = {
 };
 const args = process.argv.slice(2);
 
-export const client = await pgwire.pgconnect({
-  user: process.env.PG_USER,
-  password: process.env.PG_PASSWORD,
-  hostname: process.env.PG_HOST,
-  port: process.env.PG_PORT,
-  database: process.env.PG_DATABASE,
-});
-
 cron.schedule("*/5 * * * *", async () => {
   try {
+    const client = await pgwire.pgconnect({
+      user: process.env.PG_USER,
+      password: process.env.PG_PASSWORD,
+      hostname: process.env.PG_HOST,
+      port: process.env.PG_PORT,
+      database: process.env.PG_DATABASE,
+    });
     const current = new Date();
     const hours = df.subMinutes(current, 5);
     const end = df.format(current, "yyyy-MM-dd HH:mm:ss.SSS");
     const start = df.format(hours, "yyyy-MM-dd HH:mm:ss.SSS");
+
     console.log(`Working on ${start}  to ${end}`);
+
     const { results } = await client.query(
       `select o.uid ou,o.name,o.path,psi.programstageinstanceid::text,psi.uid,to_char(psi.created,'YYYY-MM-DD') created,to_char(psi.created,'MM') m,to_char(psi.lastupdated,'YYYY-MM-DD') lastupdated,programinstanceid::text,programstageid::text,attributeoptioncomboid::text,psi.deleted,psi.storedby,to_char(duedate,'YYYY-MM-DD') duedate,to_char(executiondate,'YYYY-MM-DD') executiondate,psi.organisationunitid::text,status,completedby,to_char(completeddate,'YYYY-MM-DD') completeddate,eventdatavalues->'bbnyNYD1wgS'->>'value' as vaccine,eventdatavalues->'LUIsbsm3okG'->>'value' as dose,assigneduserid::text,psi.createdbyuserinfo,psi.lastupdatedbyuserinfo from programstageinstance psi inner join organisationunit o using(organisationunitid) where psi.created >= '${start}' and psi.created < '${end}' and programstageid = 12715`
     );
@@ -66,5 +67,7 @@ cron.schedule("*/5 * * * *", async () => {
     console.log(`Finished working on ${start} and ${end}`);
   } catch (error) {
     console.log(error.message);
+  } finally {
+    client.end();
   }
 });
