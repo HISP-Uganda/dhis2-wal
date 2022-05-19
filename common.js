690a0a0ac97129471a8c1baa06f8de1193936596
Charles Olupot <olupotcharles@gmail.com>
2022-05-19 15:45:36 +0300

    Trying something new

--- common.js
+++ common.js
@@ -58,81 +58,45 @@ module.exports.query = `select o.uid as orgUnit,
     where ot.organisationunitid = tei.organisationunitid
   ) as regPath,
   concat(tei.uid, psi.uid) as id,
+  pi.created as "pi_created",
+  pi.lastupdated as "pi_last_updated",
+  pi.incidentdate as "pi_incident_date",
+  pi.enrollmentdate as "pi_enrollment_date",
+  pi.completedby as "pi_completed_by",
+  pi.deleted as "pi_deleted",
+  pi.storedby as "pi_stored_by",
+  pi.status as "pi_status",
+  psi.uid as "event_uid",
+  psi.created as "event_created",
+  psi.lastupdated as "event_last_updated",
+  psi.deleted as "event_deleted",
+  psi.storedby as "event_stored_by",
+  psi.duedate as "event_duedate",
+  psi.executiondate as "event_execution_date",
+  psi.status as "event_status",
+  psi.completedby as "event_completed_by",
+  psi.completeddate as "event_completed_date",
+  psi.createdbyuserinfo->>'username' as "event_created_by",
+  psi.lastupdatedbyuserinfo->>'username' as "event_lastupdated_by",
+  tei.uid as "tei_uid",
+  tei.created as "tei_created",
+  tei.lastupdated as "tei_last_updated",
+  tei.inactive as "tei_inactive",
+  tei.deleted as "tei_deleted",
+  tei.storedby as "tei_stored_by",
   (
     select jsonb_object_agg(tea.uid, value) AS months
     from trackedentityattributevalue teav
       inner join trackedentityattribute tea using(trackedentityattributeid)
     where teav.trackedentityinstanceid = tei.trackedentityinstanceid
-  ) || eventdatavalues || jsonb_build_object (
-    'pi',
-    jsonb_build_object(
-      'created',
-      pi.created,
-      'lastupdated',
-      pi.lastupdated,
-      'incidentdate',
-      pi.incidentdate,
-      'enrollmentdate',
-      pi.enrollmentdate,
-      'completedby',
-      pi.completedby,
-      'deleted',
-      pi.deleted,
-      'storedby',
-      pi.storedby,
-      'status',
-      pi.status
-    )
-  ) || jsonb_build_object(
-    'event',
-    jsonb_build_object(
-      'uid',
-      psi.uid,
-      'created',
-      psi.created,
-      'lastupdated',
-      psi.lastupdated,
-      'deleted',
-      psi.deleted,
-      'storedby',
-      psi.storedby,
-      'duedate',
-      psi.duedate,
-      'executiondate',
-      psi.executiondate,
-      'status',
-      psi.status,
-      'completedby',
-      psi.completedby,
-      'completeddate',
-      psi.completeddate,
-      'createdbyuserinfo',
-      psi.createdbyuserinfo,
-      'lastupdatedbyuserinfo',
-      psi.lastupdatedbyuserinfo
-    )
-  ) || jsonb_build_object(
-    'tei',
-    jsonb_build_object(
-      'uid',
-      tei.uid,
-      'created',
-      tei.created,
-      'lastupdated',
-      tei.lastupdated,
-      'inactive',
-      tei.inactive,
-      'deleted',
-      tei.deleted,
-      'storedby',
-      tei.storedby
-    )
-  ) as dt
+  ) as attributes,
+  eventdatavalues
 from programstageinstance psi
   inner join programstage ps using(programstageid)
   inner join organisationunit o using(organisationunitid)
   inner join programinstance pi using(programinstanceid)
-  inner join trackedentityinstance tei using(trackedentityinstanceid) where ps.uid = 'a1jCssI2LkW';`;
+  inner join trackedentityinstance tei using(trackedentityinstanceid)
+where ps.uid = 'a1jCssI2LkW';`;
 
 module.exports.intervalQuery = `select o.uid as orgUnit,
   o.path,
@@ -149,76 +113,39 @@ module.exports.intervalQuery = `select o.uid as orgUnit,
     where ot.organisationunitid = tei.organisationunitid
   ) as regPath,
   concat(tei.uid, psi.uid) as id,
+  pi.created as "pi_created",
+  pi.lastupdated as "pi_last_updated",
+  pi.incidentdate as "pi_incident_date",
+  pi.enrollmentdate as "pi_enrollment_date",
+  pi.completedby as "pi_completed_by",
+  pi.deleted as "pi_deleted",
+  pi.storedby as "pi_stored_by",
+  pi.status as "pi_status",
+  psi.uid as "event_uid",
+  psi.created as "event_created",
+  psi.lastupdated as "event_last_updated",
+  psi.deleted as "event_deleted",
+  psi.storedby as "event_stored_by",
+  psi.duedate as "event_duedate",
+  psi.executiondate as "event_execution_date",
+  psi.status as "event_status",
+  psi.completedby as "event_completed_by",
+  psi.completeddate as "event_completed_date",
+  psi.createdbyuserinfo->>'username' as "event_created_by",
+  psi.lastupdatedbyuserinfo->>'username' as "event_lastupdated_by",
+  tei.uid as "tei_uid",
+  tei.created as "tei_created",
+  tei.lastupdated as "tei_last_updated",
+  tei.inactive as "tei_inactive",
+  tei.deleted as "tei_deleted",
+  tei.storedby as "tei_stored_by",
   (
     select jsonb_object_agg(tea.uid, value) AS months
     from trackedentityattributevalue teav
       inner join trackedentityattribute tea using(trackedentityattributeid)
     where teav.trackedentityinstanceid = tei.trackedentityinstanceid
-  ) || eventdatavalues || jsonb_build_object (
-    'pi',
-    jsonb_build_object(
-      'created',
-      pi.created,
-      'lastupdated',
-      pi.lastupdated,
-      'incidentdate',
-      pi.incidentdate,
-      'enrollmentdate',
-      pi.enrollmentdate,
-      'completedby',
-      pi.completedby,
-      'deleted',
-      pi.deleted,
-      'storedby',
-      pi.storedby,
-      'status',
-      pi.status
-    )
-  ) || jsonb_build_object(
-    'event',
-    jsonb_build_object(
-      'uid',
-      psi.uid,
-      'created',
-      psi.created,
-      'lastupdated',
-      psi.lastupdated,
-      'deleted',
-      psi.deleted,
-      'storedby',
-      psi.storedby,
-      'duedate',
-      psi.duedate,
-      'executiondate',
-      psi.executiondate,
-      'status',
-      psi.status,
-      'completedby',
-      psi.completedby,
-      'completeddate',
-      psi.completeddate,
-      'createdbyuserinfo',
-      psi.createdbyuserinfo,
-      'lastupdatedbyuserinfo',
-      psi.lastupdatedbyuserinfo
-    )
-  ) || jsonb_build_object(
-    'tei',
-    jsonb_build_object(
-      'uid',
-      tei.uid,
-      'created',
-      tei.created,
-      'lastupdated',
-      tei.lastupdated,
-      'inactive',
-      tei.inactive,
-      'deleted',
-      tei.deleted,
-      'storedby',
-      tei.storedby
-    )
-  ) as dt
+  ) as attributes,
+  eventdatavalues
 from programstageinstance psi
   inner join programstage ps using(programstageid)
   inner join organisationunit o using(organisationunitid)
@@ -230,7 +157,7 @@ where ps.uid = 'a1jCssI2LkW'
     or tei.lastupdated >= LOCALTIMESTAMP - INTERVAL '3 hours 5 minutes'
     or psi.created >= LOCALTIMESTAMP - INTERVAL '3 hours 5 minutes'
     or psi.lastupdated >= LOCALTIMESTAMP - INTERVAL '3 hours 5 minutes'
-  )`;
+  );`;
 
 module.exports.monthlyBacklogQuery = (date) => `select o.uid as orgUnit,
   o.path,
@@ -374,114 +301,108 @@ module.exports.createBacklogQuery = (start, end) => {
     where ot.organisationunitid = tei.organisationunitid
   ) as regPath,
   concat(tei.uid, psi.uid) as id,
+  pi.created as "pi_created",
+  pi.lastupdated as "pi_last_updated",
+  pi.incidentdate as "pi_incident_date",
+  pi.enrollmentdate as "pi_enrollment_date",
+  pi.completedby as "pi_completed_by",
+  pi.deleted as "pi_deleted",
+  pi.storedby as "pi_stored_by",
+  pi.status as "pi_status",
+  psi.uid as "event_uid",
+  psi.created as "event_created",
+  psi.lastupdated as "event_last_updated",
+  psi.deleted as "event_deleted",
+  psi.storedby as "event_stored_by",
+  psi.duedate as "event_duedate",
+  psi.executiondate as "event_execution_date",
+  psi.status as "event_status",
+  psi.completedby as "event_completed_by",
+  psi.completeddate as "event_completed_date",
+  psi.createdbyuserinfo->>'username' as "event_created_by",
+  psi.lastupdatedbyuserinfo->>'username' as "event_lastupdated_by",
+  tei.uid as "tei_uid",
+  tei.created as "tei_created",
+  tei.lastupdated as "tei_last_updated",
+  tei.inactive as "tei_inactive",
+  tei.deleted as "tei_deleted",
+  tei.storedby as "tei_stored_by",
   (
     select jsonb_object_agg(tea.uid, value) AS months
     from trackedentityattributevalue teav
       inner join trackedentityattribute tea using(trackedentityattributeid)
     where teav.trackedentityinstanceid = tei.trackedentityinstanceid
-  ) || eventdatavalues || jsonb_build_object (
-    'pi',
-    jsonb_build_object(
-      'created',
-      pi.created,
-      'lastupdated',
-      pi.lastupdated,
-      'incidentdate',
-      pi.incidentdate,
-      'enrollmentdate',
-      pi.enrollmentdate,
-      'completedby',
-      pi.completedby,
-      'deleted',
-      pi.deleted,
-      'storedby',
-      pi.storedby,
-      'status',
-      pi.status
-    )
-  ) || jsonb_build_object(
-    'event',
-    jsonb_build_object(
-      'uid',
-      psi.uid,
-      'created',
-      psi.created,
-      'lastupdated',
-      psi.lastupdated,
-      'deleted',
-      psi.deleted,
-      'storedby',
-      psi.storedby,
-      'duedate',
-      psi.duedate,
-      'executiondate',
-      psi.executiondate,
-      'status',
-      psi.status,
-      'completedby',
-      psi.completedby,
-      'completeddate',
-      psi.completeddate,
-      'createdbyuserinfo',
-      psi.createdbyuserinfo,
-      'lastupdatedbyuserinfo',
-      psi.lastupdatedbyuserinfo
-    )
-  ) || jsonb_build_object(
-    'tei',
-    jsonb_build_object(
-      'uid',
-      tei.uid,
-      'created',
-      tei.created,
-      'lastupdated',
-      tei.lastupdated,
-      'inactive',
-      tei.inactive,
-      'deleted',
-      tei.deleted,
-      'storedby',
-      tei.storedby
-    )
-  ) as dt
+  ) as attributes,
+  eventdatavalues
 from programstageinstance psi
   inner join programstage ps using(programstageid)
   inner join organisationunit o using(organisationunitid)
   inner join programinstance pi using(programinstanceid)
-  inner join trackedentityinstance tei using(trackedentityinstanceid) where ps.uid = 'a1jCssI2LkW' and psi.created >= '${start}' and psi.created < '${end}';`;
+  inner join trackedentityinstance tei using(trackedentityinstanceid)
+where ps.uid = 'a1jCssI2LkW' and psi.created >= '${start}' and psi.created < '${end}';`;
 };
 
 module.exports.processAndInsert2 = async (index, rows) => {
   const all = rows.map(
-    ({ id, stage, stagename, dt, orgunit, path, regorgunit, regpath }) => {
-      const eventOrgUnit = _.fromPairs(
-        String(path)
-          .split("/")
-          .slice(1)
-          .map((x, i) => {
-            return [`level${i + 1}`, x];
-          })
+    ({ attributes, eventdatavalues, path, regpath, ...rest }) => {
+      const processedEvents = fromPairs(
+        Object.entries(eventdatavalues).flatMap(([dataElement, value]) => {
+          return [
+            [dataElement, value.value],
+            [`${dataElement.toLowerCase()}_created`, value.created],
+            [`${dataElement.toLowerCase()}_last_updated`, value.lastUpdated],
+            [
+              `${dataElement.toLowerCase()}_created_by`,
+              value.createdByUserInfo.username,
+            ],
+            [
+              `${dataElement.toLowerCase()}_last_updated_by`,
+              value.lastUpdatedByUserInfo.username,
+            ],
+          ];
+        })
       );
-      const registrationOrgUnit = _.fromPairs(
-        String(regpath)
-          .split("/")
-          .slice(1)
-          .map((x, i) => {
-            return [`level${i + 1}`, x];
-          })
-      );
-      let all = { id, ...dt, stage, stagename };
-      if (dt && dt.event) {
-        all = { ...all, event: { ...dt.event, ...eventOrgUnit, orgunit } };
+      rest = { ...attributes, ...processedEvents };
+      if (path) {
+        const eventOrgUnit = _.fromPairs(
+          String(path)
+            .split("/")
+            .slice(1)
+            .map((x, i) => {
+              return [`event_level${i + 1}`, x];
+            })
+        );
+
+        rest = { ...rest, ...eventOrgUnit };
+      }
+      if (regpath) {
+        const registrationOrgUnit = _.fromPairs(
+          String(regpath)
+            .split("/")
+            .slice(1)
+            .map((x, i) => {
+              return [`reg_level${i + 1}`, x];
+            })
+        );
+        rest = { ...rest, ...registrationOrgUnit };
       }
 
-      if (dt && dt.tei) {
-        all = {
-          ...all,
-          tei: { ...dt.tei, ...registrationOrgUnit, regorgunit },
-        };
+      if (stage === "a1jCssI2LkW") {
+        const createdBySameUser =
+          rest[`${String("LUIsbsm3okG").toLowerCase()}`] &&
+          rest[`${String("bbnyNYD1wgS").toLowerCase()}`];
+        rest[`${String("LUIsbsm3okG").toLowerCase()}_created_by`] ===
+          rest[`${String("bbnyNYD1wgS").toLowerCase()}_created_by`] &&
+          String(rest[`${String("LUIsbsm3okG").toLowerCase()}_created`]).slice(
+            0,
+            10
+          ) ===
+            String(
+              rest[`${String("bbnyNYD1wgS").toLowerCase()}_created`]
+            ).slice(0, 10);
+        return { ...rest, same_user: createdBySameUser };
       }
-      return all;
+      return rest;
     }
   );
   const { data } = await this.api.post(`wal/index?index=${index}`, {
