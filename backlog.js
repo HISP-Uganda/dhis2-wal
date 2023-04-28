ac7e081f3d338a1fdb5e8bfeb67edc0a6001303f
Charles Olupot <olupotcharles@gmail.com>
2023-04-28 15:53:49 +0300

    Testing errors

--- backlog.js
+++ backlog.js
@@ -8,55 +8,55 @@ dotenv.config();
 const args = process.argv.slice(2);
 
 function getDatesInRange(startDate, endDate) {
-  const date = new Date(startDate.getTime());
-  const dates = [];
-  while (date <= endDate) {
-    dates.push(new Date(date));
-    date.setDate(date.getDate() + 1);
-  }
-  return dates;
+    const date = new Date(startDate.getTime());
+    const dates = [];
+    while (date <= endDate) {
+        dates.push(new Date(date));
+        date.setDate(date.getDate() + 1);
+    }
+    return dates;
 }
 const pool = new Pool({
-  user: process.env.PG_USER,
-  password: process.env.PG_PASSWORD,
-  host: process.env.PG_HOST,
-  port: process.env.PG_PORT,
-  database: process.env.PG_DATABASE,
+    user: process.env.PG_USER,
+    password: process.env.PG_PASSWORD,
+    host: process.env.PG_HOST,
+    port: process.env.PG_PORT,
+    database: process.env.PG_DATABASE,
 });
 
 const processData = async () => {
-  const dates = getDatesInRange(new Date(args[0]), new Date(args[1]))
-    .sort(df.compareAsc)
-    .map((d) => [
-      df.format(d, "yyyy-MM-dd"),
-      df.format(df.addDays(d, 1), "yyyy-MM-dd"),
-    ]);
-  const client = await pool.connect();
-  try {
-    for (const [start, end] of dates) {
-      console.log(`Working on ${start}`);
-      const cursor = client.query(
-        new Cursor(
-          `select o.uid ou,o.name,o.path,psi.programstageinstanceid::text,psi.uid,to_char(psi.created,'YYYY-MM-DD') created,to_char(psi.created,'MM') m,to_char(psi.lastupdated,'YYYY-MM-DD') lastupdated,programinstanceid::text,programstageid::text,attributeoptioncomboid::text,psi.deleted,psi.storedby,to_char(duedate,'YYYY-MM-DD') duedate,to_char(executiondate,'YYYY-MM-DD') executiondate,psi.organisationunitid::text,status,completedby,to_char(completeddate,'YYYY-MM-DD') completeddate,eventdatavalues->'bbnyNYD1wgS'->>'value' as vaccine,eventdatavalues->'LUIsbsm3okG'->>'value' as dose,assigneduserid::text,psi.createdbyuserinfo,psi.lastupdatedbyuserinfo from programstageinstance psi inner join organisationunit o using(organisationunitid) where psi.created >= '${start}' and psi.created < '${end}' and programstageid = 12715`
-        )
-      );
+    const dates = getDatesInRange(new Date(args[0]), new Date(args[1]))
+        .sort(df.compareAsc)
+        .map((d) => [
+            df.format(d, "yyyy-MM-dd"),
+            df.format(df.addDays(d, 1), "yyyy-MM-dd"),
+        ]);
+    const client = await pool.connect();
+    try {
+        for (const [start, end] of dates) {
+            console.log(`Working on ${start}`);
+            const cursor = client.query(
+                new Cursor(
+                    `select o.uid ou,o.name,o.path,psi.programstageinstanceid::text,psi.uid,to_char(psi.created,'YYYY-MM-DD') created,to_char(psi.created,'MM') m,to_char(psi.lastupdated,'YYYY-MM-DD') lastupdated,programinstanceid::text,programstageid::text,attributeoptioncomboid::text,psi.deleted,psi.storedby,to_char(duedate,'YYYY-MM-DD') duedate,to_char(executiondate,'YYYY-MM-DD') executiondate,psi.organisationunitid::text,status,completedby,to_char(completeddate,'YYYY-MM-DD') completeddate,eventdatavalues->'bbnyNYD1wgS'->>'value' as vaccine,eventdatavalues->'LUIsbsm3okG'->>'value' as dose,assigneduserid::text,psi.createdbyuserinfo,psi.lastupdatedbyuserinfo from programstageinstance psi inner join organisationunit o using(organisationunitid) where psi.created >= '${start}' and psi.created < '${end}' and programstageid = 12715`
+                )
+            );
 
-      let rows = await cursor.read(batchSize);
-      if (rows.length > 0) {
-        await processAndInsert("programstageinstance", rows);
-      }
-      while (rows.length > 0) {
-        rows = await cursor.read(batchSize);
-        if (rows.length > 0) {
-          await processAndInsert("programstageinstance", rows);
+            let rows = await cursor.read(batchSize);
+            if (rows.length > 0) {
+                await processAndInsert("programstageinstance", rows);
+            }
+            while (rows.length > 0) {
+                rows = await cursor.read(batchSize);
+                if (rows.length > 0) {
+                    await processAndInsert("programstageinstance", rows);
+                }
+            }
+            console.log(`Finished working on ${start}`);
         }
-      }
-      console.log(`Finished working on ${start}`);
+    } catch (error) {
+        console.log(error.message);
+    } finally {
+        client.release();
     }
-  } catch (error) {
-    console.log(error.message);
-  } finally {
-    client.release();
-  }
 };
 processData().then(() => console.log("Done"));
--- schedule2.js
+++ schedule2.js
@@ -6,30 +6,36 @@ const cron = require("node-cron");
 dotenv.config();
 
 const pool = new Pool({
-  user: process.env.PG_USER_LIVE,
-  password: process.env.PG_PASSWORD_LIVE,
-  host: process.env.PG_HOST_LIVE,
-  port: process.env.PG_PORT_LIVE,
-  database: process.env.PG_DATABASE_LIVE,
+    user: process.env.PG_USER_LIVE,
+    password: process.env.PG_PASSWORD_LIVE,
+    host: process.env.PG_HOST_LIVE,
+    port: process.env.PG_PORT_LIVE,
+    database: process.env.PG_DATABASE_LIVE,
 });
 
-cron.schedule("*/5 * * * *", async () => {
-  const client = await pool.connect();
-  try {
-    const cursor = client.query(new Cursor(intervalQuery));
-    let rows = await cursor.read(batchSize);
-    if (rows.length > 0) {
-      await processAndInsert2("epivac", rows);
+const scheduleData = async () => {
+    const client = await pool.connect();
+    try {
+        const cursor = client.query(new Cursor(intervalQuery));
+        let rows = await cursor.read(batchSize);
+        if (rows.length > 0) {
+            await processAndInsert2("epivac", rows);
+        }
+        while (rows.length > 0) {
+            rows = await cursor.read(batchSize);
+            if (rows.length > 0) {
+                await processAndInsert2("epivac", rows);
+            }
+        }
+    } catch (error) {
+        console.log(error.message);
+    } finally {
+        client.release();
     }
-    while (rows.length > 0) {
-      rows = await cursor.read(batchSize);
-      if (rows.length > 0) {
-        await processAndInsert2("epivac", rows);
-      }
-    }
-  } catch (error) {
-    console.log(error.message);
-  } finally {
-    client.release();
-  }
-});
+};
+
+// cron.schedule("*/5 * * * *", async () => {
+
+// });
+
+scheduleData().then(() => console.log("Done"));
