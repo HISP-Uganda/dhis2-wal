de3052389cff532a0e7b8f1038888ff14433f7b2
Charles Olupot <olupotcharles@gmail.com>
2023-08-15 11:14:08 +0300

    Change database

--- transfer.js
+++ transfer.js
@@ -7,27 +7,27 @@ const dotenv = require("dotenv");
 dotenv.config();
 
 const pool = new Pool({
-  user: process.env.PG_USER,
-  password: process.env.PG_PASSWORD,
-  host: process.env.PG_HOST,
-  port: process.env.PG_PORT,
-  database: process.env.PG_DATABASE,
+    user: process.env.PG_USER_LIVE,
+    password: process.env.PG_PASSWORD_LIVE,
+    host: process.env.PG_HOST_LIVE,
+    port: process.env.PG_PORT_LIVE,
+    database: process.env.PG_DATABASE_LIVE,
 });
 
 const transferAll = async (index) => {
-  const client = await pool.connect();
-  const cursor = client.query(new Cursor(query));
-  let rows = await cursor.read(batchSize);
-  if (rows.length > 0) {
-    await processAndInsert2(index, rows);
-  }
-  while (rows.length > 0) {
-    rows = await cursor.read(batchSize);
+    const client = await pool.connect();
+    const cursor = client.query(new Cursor(query));
+    let rows = await cursor.read(batchSize);
     if (rows.length > 0) {
-      await processAndInsert2(index, rows);
+        await processAndInsert2(index, rows);
     }
-  }
-  cursor.close(() => client.release());
+    while (rows.length > 0) {
+        rows = await cursor.read(batchSize);
+        if (rows.length > 0) {
+            await processAndInsert2(index, rows);
+        }
+    }
+    cursor.close(() => client.release());
 };
 
 transferAll("epivac").then(() => console.log("Done"));
